f4df553fbb0bb5c65f0df64e541cd561aec74508
Bart van 't Ende <bartvtende@gmail.com>
2015-03-25 12:06:49 +0100

    Test Travis CI automated tests

--- src/main/java/org/hanzet23/gameframework/App.java
+++ src/main/java/org/hanzet23/gameframework/App.java
@@ -4,10 +4,8 @@ package main.java.org.hanzet23.gameframework;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
