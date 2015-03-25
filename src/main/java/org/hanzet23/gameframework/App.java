84a4aff8e1c73bee10787c769bd427829d72cef0
Bart van 't Ende <bartvtende@gmail.com>
2015-03-25 12:04:32 +0100

    Initial commit?

---
+++ src/main/java/org/hanzet23/gameframework/App.java
@@ -0,0 +1,13 @@
+package main.java.org.hanzet23.gameframework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
