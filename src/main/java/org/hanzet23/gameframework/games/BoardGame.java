e3911b227cfa12474fdb4e95633163de36997d37
Ivonov <j.reijneveld@gmail.com>
2015-03-26 13:45:19 +0100

    Methodes aan Gamemodels toegevoegd inclusief TODO list

---
+++ src/main/java/org/hanzet23/gameframework/games/BoardGame.java
@@ -0,0 +1,60 @@
+package main.java.org.hanzet23.gameframework.games;
+
+/**
+ * Dit is de abstracte klasse is de super van alle games die worden ontwikkeld in deze framework
+ * In dit object worden basis functies gedefineerd die iedere board game moet bevatten.
+ * Methodes die specifiek zijn aan de game zelf worden verder gedefineerd in de game klassen zelf. 
+ * Created by Joz Reijnevweld on 26/03/15.
+ */
+public abstract class BoardGame {
+    private char[][] board;
+    private int playerState = -1; // -1 staat voor dat er nog geen player is geselecteerd
+
+    public BoardGame(){  
+        //TODO BoardGame moet nog geimplementeerd worden 
+        //Moet gechecked worden welke parameters nodig zijn voor communicatie tussen objecten.
+    }
+
+    /**
+     * Deze methode reset of print de board.
+     * Dit komt voor als het spel start of voorbij is. 
+     */
+    public void SetResetGameBoard(){ 
+        //TODO SetResetGameBoard moet nog geimplementeerd worden 
+    }
+
+    /**
+     * Deze methode print het board in de meest recente status
+     * Dit word aangeroepen wanneer een verandering is gemaakt aan het board situatie. 
+     */
+    public void PrintCurrentBoard(){ 
+        //TODO PrintCurrentBoard moet nog geimplementeerd worden
+    }
+
+    /**
+     * Deze methode checked of de board volledig is gevult.
+     * Indien dit het geval is dan return deze een true. 
+     */
+    public boolean CheckIfBoardIsFull(){ return true; }
+
+    /**
+     * Deze methode checked of er een winnaar is.
+     * Om te checken of er een winnaar is, zal deze gebruik maken van andere methodes
+     * die specifiek zijn aan de desbetreffende board game.
+     * Er word verwacht dat deze methode word @Override 
+     */
+    public abstract boolean CheckIfThereIsAWin();
+
+    /**
+     * Deze methode veranderd de status van een speler.
+     * Dit houd in wanneer een speler aan de beurt is.
+     */
+    public void ChangePlayerState(){  
+        //TODO ChangePlayerState moet nog geimplementeerd worden
+    }
+
+    /**
+     * Deze methode zal een markering zetten in de board
+     */
+    public abstract void setMarker();
+}
--- src/main/java/org/hanzet23/gameframework/games/tictactoe/TicTacToeModel.java
+++ src/main/java/org/hanzet23/gameframework/games/tictactoe/TicTacToeModel.java
@@ -1,5 +1,47 @@
 package main.java.org.hanzet23.gameframework.games.tictactoe;
 
-public class TicTacToeModel {
+import main.java.org.hanzet23.gameframework.games.BoardGame;
 
+/**
+ * Dit is een child klasse die de tictactoe game functies bevalt
+ * Created by Joz Reijnevweld on 26/03/15.
+ */
+public class TicTacToeModel extends BoardGame {
+    /**
+     * Deze methode check of een player een win heeft in het diagonale.
+     * @return boolean wanneer er een win is
+     */
+    private boolean checkDiagnalWin(){
+        //TODO CheckDiagnalWin moet nog geimplementeerd worden.
+        return false;
+    }
+
+    /**
+     * Deze methode checked of een player een win heeft in het horizontale. 
+     * @return boolean wanneer er een win is.
+     */
+    private boolean checkHorizontalWin(){
+        //TODO CheckHorizontalWin moet nog geimplementeerd worden.
+        return false;
+    }
+
+    /**
+     * Deze methode checked of een player een win heeft in het verticale. 
+     * @return boolean wanneer er een win is.
+     */
+    private boolean checkVerticalWin(){
+        //TODO CheckVerticalWin moet nog geimplementeerd worden.
+        return false;
+    }
+
+    @Override
+    public boolean CheckIfThereIsAWin() {
+        if(checkDiagnalWin() && checkHorizontalWin() && checkVerticalWin()){ return true; }
+        else { return false; }
+    }
+
+    @Override
+    public void setMarker() {
+        //TODO setMarker moet nog geimplementeerd worden
+    }
 }
