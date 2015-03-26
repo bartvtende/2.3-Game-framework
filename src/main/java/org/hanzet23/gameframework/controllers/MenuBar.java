119f226d1feb2e7e06b3949f79538a4fd048e862
Figueus <j-c-s-s@hotmail.com>
2015-03-26 14:01:06 +0100

    Menubar modularisatie

---
+++ src/main/java/org/hanzet23/gameframework/controllers/MenuBar.java
@@ -0,0 +1,52 @@
+package main.java.org.hanzet23.gameframework.controllers;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+public class MenuBar extends JMenuBar {
+
+	public MenuBar(){
+	//maak menu 1
+    JMenu file = new JMenu("File");
+    
+    
+    //Menu items
+    JMenuItem newConnection = new JMenuItem("New connection");
+    JMenuItem newGame = new JMenuItem("New game");
+    
+    newConnection.addActionListener(new ActionListener(){
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			// TODO Auto-generated method stub
+			JFrame popup = new NewConnection();
+			popup.setVisible(true);
+			popup.pack();
+		}
+    	
+    });
+    
+    newGame.addActionListener(new ActionListener(){
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			JFrame popup = new NewGame();
+			popup.setVisible(true);
+			popup.pack();
+		}
+    	
+    });
+    
+    file.add(newConnection);
+    file.add(newGame);
+    
+    this.add(file);
+    
+	}
+}
--- src/main/java/org/hanzet23/gameframework/views/GameFrame.java
+++ src/main/java/org/hanzet23/gameframework/views/GameFrame.java
@@ -10,6 +10,7 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
 import main.java.org.hanzet23.gameframework.controllers.GameControls;
+import main.java.org.hanzet23.gameframework.controllers.MenuBar;
 import main.java.org.hanzet23.gameframework.controllers.NewConnection;
 import main.java.org.hanzet23.gameframework.controllers.NewGame;
 
@@ -21,52 +22,10 @@ public class GameFrame extends JFrame {
 	public GameFrame(){
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		this.setJMenuBar(createMenu());
+		this.setJMenuBar(new MenuBar());
 		this.add(gamePanel, BorderLayout.CENTER);
 		this.add(new GameControls(), BorderLayout.EAST);
 	}
-	private JMenuBar createMenu(){
-		//maak JMenuBar
-        JMenuBar menu = new JMenuBar();
-        
-        //maak menu 1
-        JMenu file = new JMenu("File");
-        
-        
-        //Menu items
-        JMenuItem newConnection = new JMenuItem("New connection");
-        JMenuItem newGame = new JMenuItem("New game");
-        
-        newConnection.addActionListener(new ActionListener(){
-
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				// TODO Auto-generated method stub
-				JFrame popup = new NewConnection();
-				popup.setVisible(true);
-				popup.pack();
-			}
-        	
-        });
-        
-        newGame.addActionListener(new ActionListener(){
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				JFrame popup = new NewGame();
-				popup.setVisible(true);
-				popup.pack();
-			}
-        	
-        });
-        
-        file.add(newConnection);
-        file.add(newGame);
-        
-        menu.add(file);
-        
-        return menu;
-	}
+	
 
 }
