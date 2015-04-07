7430fd30e797126f2d7b557b215645d0c68b3ae7
Figueus <j-c-s-s@hotmail.com>
2015-04-07 21:07:01 +0200

    Circle shape is made

---
+++ src/main/java/org/hanzet23/gameframework/games/tictactoe/CircleShape.java
@@ -0,0 +1,50 @@
+package main.java.org.hanzet23.gameframework.games.tictactoe;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+
+public class CircleShape extends JPanel {
+
+	private double[] PointsX = {0.1, 0.9, 0.23, 0.77};
+	private double[] PointsY = {0.1, 0.9, 0.23, 0.77};
+	private Color background;
+	private Color circle;
+	
+	public CircleShape(int x, int y, Color circle, Color background){
+		updatePoints(x,y);
+		this.background = background;
+		this.circle = circle;
+		
+		this.setPreferredSize(new Dimension(x,y));
+	}
+	
+	private void doDrawing(Graphics g){
+		g.setColor(background);
+		this.setBackground(background);
+		g.setColor(circle);
+		
+		g.fillOval(new Double(PointsX[0]).intValue(), new Double(PointsY[0]).intValue() , new Double(PointsX[1]-PointsX[0]).intValue(), new Double(PointsY[1]-PointsY[0]).intValue());
+		g.setColor(background);
+		g.fillOval(new Double(PointsX[2]).intValue(), new Double(PointsY[2]).intValue() , new Double(PointsX[3]-PointsX[2]).intValue(), new Double(PointsY[3]-PointsY[2]).intValue());
+	}
+	
+	private void updatePoints(int x, int y){
+		for(int i = 0; i<PointsX.length;i++){
+			PointsX[i] = PointsX[i] * x;
+		}
+		
+		for(int i = 0; i<PointsY.length;i++){
+			PointsY[i] = PointsY[i] * y;
+		}
+	}
+	
+	public void paintComponent(Graphics g) {
+
+		super.paintComponent(g);
+		doDrawing(g);
+	}
+}
