be0fd26df8fa1a0d7ab18f1ffe5010769010e47e
Figueus <j-c-s-s@hotmail.com>
2015-04-08 11:49:01 +0200

    Renamed and put tiles in a matrix

---
+++ src/main/java/org/hanzet23/gameframework/games/tictactoe/BoardView.java
@@ -0,0 +1,28 @@
+package main.java.org.hanzet23.gameframework.games.tictactoe;
+
+import java.awt.Dimension;
+import java.awt.GridLayout;
+
+import javax.swing.JPanel;
+
+public class BoardView extends JPanel {
+
+	private Tile[][] tiles = new Tile[3][3];
+	
+	public BoardView(){
+		this.setLayout(new GridLayout(3,3));
+		for(int i = 0; i<3;i++){
+			for(int j = 0; j<3;j++){
+				Tile tile = new Tile();
+				tiles[i][j] = tile;
+				this.add(tile);
+			}
+			
+			
+		}
+		
+		this.setPreferredSize(new Dimension(150, 150));
+	}
+	
+	
+}
