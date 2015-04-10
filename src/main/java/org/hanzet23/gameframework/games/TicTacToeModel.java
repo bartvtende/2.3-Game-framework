898e16fc4981af22a460a694edae3f9905e04cea
Figueus <j-c-s-s@hotmail.com>
2015-04-10 18:33:22 +0200

    Refresh method in BoardView

--- src/main/java/org/hanzet23/gameframework/games/TicTacToeModel.java
+++ src/main/java/org/hanzet23/gameframework/games/TicTacToeModel.java
@@ -1,17 +1,30 @@
 package main.java.org.hanzet23.gameframework.games;
 
+import javax.swing.JFrame;
+
+import main.java.org.hanzet23.gameframework.games.tictactoe.BoardView;
+
 public class TicTacToeModel extends GameModel {
 	
 	private final int BOARD_RANGE = 3;
+	private JFrame boardFrame;
+	private BoardView boardView;
+	public static TicTacToeModel TTTModel;
 
 	public TicTacToeModel(String gameName) {
 		super(gameName);
 		this.board = new char[BOARD_RANGE][BOARD_RANGE];
+		TTTModel = this;
 	}
 
 	@Override
 	public void moveHuman() {
 		// Luister naar de actionlisteners
+		for(int i = 0; i<BOARD_RANGE;i++){
+			for(int j = 0; j<BOARD_RANGE; i++){
+				boardView.getTile(i, j).setEnabled(true);
+			}
+		}
 	}
 
 	@Override
@@ -22,11 +35,28 @@ public class TicTacToeModel extends GameModel {
 	@Override
 	public void startGame() {
 		// Maak view
+		boardFrame = new JFrame();
+		boardView = new BoardView();
+		boardFrame.getContentPane().add(boardView);
+		
+		boardFrame.setVisible(true);
+		boardFrame.pack();
+		//yea, yea, hardcoded vars, i know, bad me
+		boardFrame.getContentPane().setSize(BOARD_RANGE*50, BOARD_RANGE*50);
+		boardFrame.setResizable(false);
 	}
 
 	@Override
 	public void stopGame() {
 		// Remove the view
+		if(boardFrame != null){
+			boardFrame.dispose();
+			boardFrame = null;
+		}
+	}
+	
+	public BoardView getBoardView(){
+		return BoardView;
 	}
 	
 }
--- src/main/java/org/hanzet23/gameframework/games/tictactoe/BoardView.java
+++ src/main/java/org/hanzet23/gameframework/games/tictactoe/BoardView.java
@@ -26,4 +26,43 @@ public class BoardView extends JPanel {
 	public Tile getTile(int x, int y){
 		return tiles[x][y];
 	}
+	
+	/**
+	 * Makes the GUI board based on the matrix that is passed as a parameter
+	 * The value matrix must be filled with the values 'X', 'x', 'O', 'o', 'E' or 'e'.
+	 * @param board
+	 */
+	public void refresh(char[][] board){
+		this.removeAll();
+		
+		for(int i = 0; i<board.length; i++){
+			for(int j = 0; j>board[0].length; j++){
+				char boardChar = board[i][j];
+				if(boardChar == 'X' || boardChar == 'x'){
+					Tile tile = new Tile();
+					tile.setContent("X");
+					
+					tiles[i][j] = tile;
+					this.add(tile);
+					
+				}
+				else if(boardChar == 'O'||boardChar == 'o'){
+					Tile tile = new Tile();
+					tile.setContent("O");
+					
+					tiles[i][j] = tile;
+					this.add(tile);
+				}
+				else if(boardChar == 'E'||boardChar == 'e'){
+					Tile tile = new Tile();
+					tile.setContent("Empty");
+					
+					tiles[i][j] = tile;
+					this.add(tile);
+				}
+			}
+		}
+		
+		this.revalidate();
+	}
 }
